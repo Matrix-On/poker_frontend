ac6554a8c48d8957a0c7564401363b068c4b5f8f
Matrix-On <klon.sa@yandex.ru>
2025-01-06 03:00:54 +0300

    build

--- build/Qt_6_7_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_apppoker_tournament_view_FILE /Users/artemsamujlik/Development/poker_tournament_view/build/Qt_6_7_2_for_macOS-Debug/apppoker_tournament_view.app/Contents/MacOS/apppoker_tournament_view)
-set(__QT_DEPLOY_TARGET_apppoker_tournament_view_TYPE EXECUTABLE)
